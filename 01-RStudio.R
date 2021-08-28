e12ec7019df36e62d4fd2b177bd2c9d3c50f9fb8
GustavoBerlitz <89302447+GustavoBerlitz@users.noreply.github.com>
2021-08-27 23:58:11 -0300

    Aula01

---
+++ 01-RStudio.R
@@ -0,0 +1,37 @@
+## Configurando diretório
+
+getwd()
+
+## Contribuidores
+contributors()
+
+print("Bom sábado")
+
+## criar um gráfico
+plot(1:25)
+
+## Instalar pacotes
+
+installed.packages()
+install.packages("randomForest")
+
+## Carregando pacotes
+library(randomForest)
+
+## Descarregando pacotes
+detach(package:randomForest)
+
+## Ajuda
+help("detach")
+
+?detach
+??detach
+
+install.packages("sos")
+
+library(sos)
+
+findFn("detach")
+
+##kaggle - Competições para resolver problemas das empresas e bom p achar emprego
+
