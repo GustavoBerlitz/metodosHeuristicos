a56724327ab7fd0cc045ecdb1d7aefb11dc9baf6
GustavoBerlitz <89302447+GustavoBerlitz@users.noreply.github.com>
2021-08-28 09:14:28 -0300

    Aula02

---
+++ Scripts/02-Operadores.R
@@ -0,0 +1,38 @@
+# Operadores básicos, relacionais e lógicos em R
+
+#Operadores básicos
+
+5+5
+7-3
+5/3
+3^2
+3**2
+16%%3
+
+#operadores relacionais
+
+x <- 7
+y <- 5
+x + y
+x <- -3
+x
+
+#Operadores
+
+x>7
+x<8
+x <= 8
+x >= 8
+x == 8
+x != 8
+
+#operadores lógicos
+
+#and
+(x==8) & (x==6)
+(x==7) & (x<=3)
+(x>-5) & (x<=3)
+
+#or
+(x>0) | (x==-3)
+
